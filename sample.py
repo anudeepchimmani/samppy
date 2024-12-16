47237995d596dcd36fc8e00103c6b3f84c42a514
Anudeep <anudeepchimmani@gmail.com>
2024-12-16 21:50:00 +0530

    this is sample python code

---
+++ sample.py
@@ -0,0 +1,11 @@
+
+# Simple Python code to add two numbers
+num1 = 5
+num2 = 7
+
+# Adding the two numbers
+sum_result = num1 + num2
+
+# Printing the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
